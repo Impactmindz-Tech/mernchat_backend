f735c596bea82ae09adc355d77e7e6843101e41a
Impactmindz-Tech <impactmindztechsolutions@gmail.com>
2024-08-21 17:16:01 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const cors = require("cors");
+const { connectDB } = require("./db/config");
+const Users = require("./models/Users");
+const bcryptjs = require("bcryptjs");
+
+const app = express();
+const port = (process.env.PORT = 8000);
+
+connectDB();
+app.use(express.json());
+app.use(cors());
+// routes
+
+app.use(express.urlencoded({ extended: false }));
+app.get("/", (req, res) => {
+  res.send("welcome");
+});
+
+app.post("/api/register", async (req, res) => {
+  try {
+    const { fullname, email, password } = req.body;
+    if (!fullname || !email || !password) {
+      return res.status(400).send("Please fill all required fields");
+    }
+
+    const alreadyExists = await Users.findOne({ email });
+    if (alreadyExists) {
+      return res.status(400).send("User already exists");
+    }
+    const hash = bcryptjs.hashSync(password, 10);
+    const user = new Users({
+      email: email,
+      password: hash,
+      fullname: fullname,
+    });
+
+    await user.save();
+    return res.status(200).send({ user, message: "User registered successfully" });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send("Server error");
+  }
+});
+
+app.post("/api/login", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    if (!email || !password) {
+      res.status(400).send("Please fill all required fields");
+    } else {
+      const user = await Users.findOne({ email });
+      if (!user) {
+        return res.status(400).send("User already exists");
+      } else {
+        const validator = await bcryptjs.compare(password, user.password);
+        if (!validator) {
+          res.status(400).send("email or password incorrect");
+        } else {
+          res.status(200).send({ user, message: "login successfuly" });
+        }
+      }
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send("Server error");
+  }
+});
+
+app.listen(port, () => {
+  console.log("listening on port " + port);
+});
---
+++ db/config.js
@@ -0,0 +1,31 @@
+// backend/config/db.js
+const mongoose =  require("mongoose")
+
+const connectDB = async () => {
+    const mongoURI = 'mongodb://localhost:27017/mernchat'; // replace with your MongoDB URI
+
+    try {
+        await mongoose.connect(mongoURI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log('Connected to MongoDB');
+    } catch (err) {
+        console.error('Failed to connect to MongoDB', err);
+        process.exit(1); // Exit process with failure
+    }
+
+    // mongoose.connection.on('connected', () => {
+    //     console.log('Mongoose connected to MongoDB');
+    // });
+
+    // mongoose.connection.on('error', (err) => {
+    //     console.error('Mongoose connection error:', err);
+    // });
+
+    // mongoose.connection.on('disconnected', () => {
+    //     console.log('Mongoose disconnected from MongoDB');
+    // });
+};
+
+module.exports = { connectDB };
